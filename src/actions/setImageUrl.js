b4e18bcfc2e20f5db48fd874ffca7344a934a42b
John Morris <john.morris@bbc.co.uk>
2019-10-25 10:44:37 +0100

    add pics

--- src/actions/setImageUrl.js
+++ src/actions/setImageUrl.js
@@ -1,5 +1,6 @@
 import { SET_IMAGE_URL, RESET_TIMER } from './types'
 import Unsplash from 'unsplash-js';
+
 const unsplash = new Unsplash({
     accessKey: "c57425732dd1b75bebcd3da7fcb27fef01be7cb74b1470d6b28ab33a4583b870",
     headers: {
@@ -9,22 +10,22 @@ const unsplash = new Unsplash({
 })
 
 export const setImageUrl = () => {
-    return (dispatch) => {
-        unsplash.photos.getRandomPhoto()
-            .then(res => res.json())
-            .then(json => dispatch({
-                type: SET_IMAGE_URL,
-                payload: json.urls.full
-            }))
+    let src = require(`./../data/pics/${Math.floor(Math.random() * 14) + 1}.jpeg`)
 
-        // dispatch({
-        //     type: SET_IMAGE_URL,
-        //     payload: 'test.png'
-        // })
+    return (dispatch) => {
+        // unsplash.photos.getRandomPhoto()
+        //     .then(res => res.json())
+        //     .then(json => dispatch({
+        //         type: SET_IMAGE_URL,
+        //         payload: json.urls.full
+        //     }))
+        dispatch({
+            type: SET_IMAGE_URL,
+            payload: src
+        })
         dispatch({
             type: RESET_TIMER,
             payload: Math.random()
         })
-
     }
 }
